330e58cfe0666cbdc217c20e6837bab792a0ac1b
Vadon Dániel <vadondaniel02@gmail.com>
2024-12-10 23:54:52 +0100

    output_timeout increased so that some tests don't fail

--- final/ticket-service-acceptance-tests/src/test/java/com/training/epam/ticketservice/at/GenericCliProcessStepDefs.java
+++ final/ticket-service-acceptance-tests/src/test/java/com/training/epam/ticketservice/at/GenericCliProcessStepDefs.java
@@ -13,7 +13,7 @@ import java.util.concurrent.TimeoutException;
 
 public class GenericCliProcessStepDefs {
 
-    private static final int OUTPUT_TIMEOUT = 30000;
+    private static final int OUTPUT_TIMEOUT = 300000;
 
     private ProcessUnderTest cliProcess;
 
